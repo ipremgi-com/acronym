7e67156e93646be2ea146e4b524b8f2505ac4b0a
Premgi <ivan.premgi@capgemini.com>
2017-03-29 20:11:37 +0100

    test and basic class created

---
+++ src/main/java/Acronym.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IPREMGI on 29/03/2017.
+ */
+public class Acronym {
+
+    private String phrase;
+
+    public Acronym(String phrase){
+        this.phrase = phrase;
+    }
+
+
+}
