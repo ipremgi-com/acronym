1134f2a5c0dbffd34916c011e05c95ebf548b738
Premgi <ivan.premgi@capgemini.com>
2017-03-29 20:24:27 +0100

    method created to identify first letter acronym

--- src/main/java/Acronym.java
+++ src/main/java/Acronym.java
@@ -22,9 +22,37 @@ public class Acronym {
         String[] words = phrase.split("\\s");
 
         for(String word: words){
+            sb.append(getAcronym(word));
+        }
+        return sb.toString().toUpperCase();
+    }
+
+
+    /**
+     * Get Acronym for each word
+     * 1) If only first letter is Upper case or whole word is upper case then return first letter
+     * 2) Break word into chars and check if there are more than 1 acronym in the word e.g. HyperText or metal-oxide
+     * @param word - create acronym for
+     * @return - the acronym for the word
+     */
+    private String getAcronym(String word){
+        StringBuilder sb = new StringBuilder();
+
+        if(word.substring(1, word.length()).matches("[a-z]*")){
             sb.append(word.substring(0,1));
         }
+
+
+
+
         return sb.toString();
+
+
+
+
     }
+
+
+
 
 }
